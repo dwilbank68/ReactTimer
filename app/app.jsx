61a432418442ed032c37839d17f775773659e6db
David Wilbanks <dwilbank@gmail.com>
2016-09-07 16:37:25 -0400

    add navigation

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Route, Router, IndexRoute, hashHistory} from 'react-router';
 import Main from 'Main';
+import Timer from 'Timer';
+import Countdown from 'Countdown';
 
 // load foundation
 require('style!css!foundation-sites/dist/foundation.min.css');
@@ -13,6 +15,8 @@ require('style!css!sass!applicationStyles');
 ReactDOM.render(
     <Router history={hashHistory}>
         <Route path="/" component={Main}>
+            <IndexRoute   component={Timer}></IndexRoute>
+            <Route path="countdown" component={Countdown}></Route>
         </Route>
     </Router>,
     document.getElementById('app')
---
+++ app/components/Countdown.jsx
@@ -0,0 +1,13 @@
+import React, {
+    PropTypes,
+} from 'react';
+
+const Countdown = React.createClass({
+    render() {
+        return (
+            <div>Countdown.jsx</div>
+        );
+    }
+});
+
+export default Countdown;
