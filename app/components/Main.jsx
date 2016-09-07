73f86634fcec7f7c7afffc7f98d1363da03ef50a
David Wilbanks <dwilbank@gmail.com>
2016-09-07 14:11:57 -0400

    initial commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navigation from 'Navigation';
+
+const Main = (props) => {
+    return (
+        <div>
+            <Navigation></Navigation>
+            <div className="row">
+                <div className="columns medium-6 large-4 small-centered">
+                    <p>Main.jsx rendered</p>
+                    {props.children}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Main;
---
+++ app/components/Navigation.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {IndexLink, Link} from 'react-router';
+
+const Navigation = (props) => {
+    return (
+        <div className="top-bar">
+            <div className="top-bar-left">
+                <ul className="menu">
+                    <li className="menu-text">React Timer App</li>
+                    <li>
+                        <IndexLink to="/"           activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink>
+                    </li>
+                    <li>
+                        <Link to="/"        activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Countdown</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="top-bar-right">
+                <ul className="menu">
+                    <li>
+                        <p className="menu-text">Created by <span>David Wilbanks</span></p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navigation;
